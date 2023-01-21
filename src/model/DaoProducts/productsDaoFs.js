3cc53c26e9546e3b6a4ec685dadd9a58269453ce
Andrés Nicolás Rubio <87342325+andresnrubio@users.noreply.github.com>
2023-01-21 13:12:13 -0300

    Delete src directory

--- src/model/DaoProducts/productsDaoFs.js
+++
@@ -1,9 +0,0 @@
-import containerFs from "../../containers/containerFs.js";
-
-class productsDaoFs extends containerFs {
-    constructor(path, model) {
-        super(path, model)
-    }
-}
-
-export default productsDaoFs
